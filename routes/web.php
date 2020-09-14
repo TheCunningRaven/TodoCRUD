d527e7acb91f3ef0d87d82bbc1a2ee83af4f57ca
Blaine <blainemoser@outlook.com>
2020-09-14 16:20:17 +0200

    Updated controller methods to use Laravel 8 standard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * TODOs routes
+ */
+Route::get('todos', [Controllers\TodosController::class, 'index']);
